7f640c5186ef8726a8a9dbb14f24a8bf585d8e7a
André Filipe <andre.r.flip@gmail.com>
2015-08-31 21:19:51 +0100

    Separeted the loop structure from the encode method

--- src/Type/Ini.php
+++ src/Type/Ini.php
@@ -7,13 +7,25 @@ class Ini implements TypeInterface
     /**
      * Encodes an array to ini strutcture
      * @param  array $data   Data to encode
-     * @param  array $parent Parent array, in case of recursive arrays
      * @return string
      */
-    public static function encode($data, array $parent = array())
+    public static function encode($data)
     {
         $data = (array) $data;
 
+        $resultString = self::loopEncode($data);
+
+        return trim($resultString, PHP_EOL);
+    }
+
+    /**
+     * Method to look the data and encode it to ini format
+     * @param  array $data   Data to encode
+     * @param  array $parent Parent array, in case of recursive arrays
+     * @return string
+     */
+    public static function loopEncode($data, array $parent = array())
+    {
         $resultString = '';
         foreach ($data as $key => $value) {
             if (is_array($value)) {
@@ -21,15 +33,15 @@ class Ini implements TypeInterface
                 //merge all the sections into one array...
                 $sec = array_merge((array) $parent, (array) $key);
                 //add section information to the output
-                $resultString .= '[' . join('.', $sec) . ']' . PHP_EOL;
+                $resultString .= PHP_EOL . '[' . join('.', $sec) . ']' . PHP_EOL;
                 //recursively traverse deeper
-                $resultString .= self::encode($value, $sec);
+                $resultString .= self::loopEncode($value, $sec);
             } else {
                 $resultString .= "$key = $value" . PHP_EOL;
             }
         }
 
-        return trim($resultString, PHP_EOL);
+        return $resultString;
     }
 
     /**
